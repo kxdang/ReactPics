a935cc07862fd9b91efdcac1f378b9343c87c2c1
kxdang <ikien.dang@gmail.com>
2019-01-27 08:52:31 -0500

    git ignore is now working

--- src/api/unsplash.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios';
-
-export default axios.create({
-    baseURL: "https://api.unsplash.com",
-    headers: {
-        Authorization: 'Client-ID 45f58661f7d11af26ea32425c0531aa06b554b063306dd18490e4d267200afc4'
-    }
-
-});
